276111a7f51e3a7249cf994a84190008960e82ee
christopherzulu0 <christopherzulu04@gmail.com>
2025-07-18 09:34:06 +0200

    files removed

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const ussdRoute = require("./index");
 const bodyParser = require("body-parser");
-const i18n = require("i18n");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-i18n.configure({
-  defaultLocale: "en", // Set the default language
-  locales: ["en", "fr", "es", "bem", "nya"], // Available languages
-  directory: __dirname + "/locales", // Path to the locales folder
-  objectNotation: true, // Enable object notation
-});
-
-app.use(i18n.init);
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", ussdRoute);
 
-module.exports = app; // Export the app for Vercel
+app.listen(3000, () => console.log(`running on localhost:3000`));
